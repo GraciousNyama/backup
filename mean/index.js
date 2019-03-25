d868f388613bc74fb5c1b85e74f0ee7533b4f078
Impact Hub #014 <nyamagoromondogracious@gmail.com>
2019-03-25 13:14:03 +0200

    object recap

--- mean/index.js
+++ mean/index.js
@@ -5,5 +5,5 @@ console.log(three);
 var four = [1,2 ,3 ,4 ,5, 6,7 , 8, 9,10]
 
 for(i=0; i<four.length; i++){
-	console.log("the index is" + i +"the value is"+ four[i]);
+	console.log("the index is  " + i +" the value is "+ four[i]);
 }
